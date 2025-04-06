6beaff1d227bad8e837dbf8ec3a8b5eed4e0ca98
Jankusnot <janek.staw@gmail.com>
2025-04-06 10:33:59 +0200

    curl

--- Pogoda/Pogoda.cpp
+++ Pogoda/Pogoda.cpp
@@ -2,19 +2,60 @@
 //
 
 #include <iostream>
+#include <locale.h>
+#include <curl/curl.h>
+
+
+// Funkcja przetwarzająca dane z CURL
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	size_t bytes = size * nmemb;
+	
+	// (string*)userp rzutowanie void* na string
+	// (char*)contents rzutowanie void* na char*
+	// append(..., size * nmemb) dodaj dokładnie tyle bajtów ile danych zostanie odebranych
+	((std::string*)userp)->append((char*)contents, bytes);
+	return bytes;
+}
+
+// Funkcja wykonująca zapytanie CURL
+bool performCurlRequest(const std::string& url, std::string& response) {
+	CURL* curl = curl_easy_init(); // Inicjalizacja CURL
+
+	if (!curl) {
+		std::cerr << "Failed to initialize CURL" << std::endl;
+		return false;
+	}
+
+	curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // Ustawienie url do którego CURL ma wysłać zapytanie, konwersja string na char*
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback); // wysłanie pobranych danych do funkcji
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response); // przekazanie response do funkcji WriteCallback jako userp
+
+	CURLcode res = curl_easy_perform(curl); //	Wysyła zapytanie o status
+	curl_easy_cleanup(curl); // Zwalnianie zasobów CURL
+
+	if (res != CURLE_OK) {
+		std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+		return false;
+	}
+	return true;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_CTYPE, "Polish"); // Poprawne wyświetlanie polskich zanaków
+
+	//std::string api_url = "google.com";
+	std::string api_url = "https://api.gios.gov.pl/pjp-api/rest/station/findAll";
+	std::string response;
+
+	curl_global_init(CURL_GLOBAL_DEFAULT); // Globalna inicjalizacja CURL
+
+	if(performCurlRequest(api_url, response)) {
+		std::cout << response;
+	}
+
+	curl_global_cleanup(); // Globalne zwalnianie zasobów CURL
+	return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
